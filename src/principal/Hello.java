3239a2fc089905e72b2fe1cc97f14af1668ad773
AthilaMatheusBorges <athila.borges@ccc.ufcg.edu.br>
2018-02-22 11:00:40 -0300

    1.1

--- src/principal/Hello.java
+++ src/principal/Hello.java
@@ -3,7 +3,7 @@ package principal;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Marcelo é bi");
+		System.out.println("Marcelo é bitolaaaaaaaaa");
 	}
 
 }

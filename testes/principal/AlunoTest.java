763e41ad02b89369d3e97e40ea07c5f045444cba
marcelofsa <marcelofsa@lcc2-14.lcc.ufcg.edu.br>
2018-02-27 14:22:59 -0300

    Criação da pasta de testes de unidade

---
+++ testes/principal/AlunoTest.java
@@ -0,0 +1,69 @@
+package principal;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AlunoTest {
+
+	@Test
+	public void testAluno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTipo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTipo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMatricula() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNome() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTelefone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCodigoCurso() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNota() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetNota() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompareTo() {
+		fail("Not yet implemented");
+	}
+
+}
